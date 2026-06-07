028c9c75d43923857e4d9e877eda9ef63c453eb0
Aleksandr Kopeikin <sundstorm91@gmail.com>
2026-06-07 11:08:11 +0700

    test: проверяем husky

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
+  const uselessVar = 'Я не используюсь'
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -17,20 +18,20 @@ export default function Home() {
             To get started, edit the page.tsx file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            Looking for a starting point or more instructions? Head over to{' '}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               Templates
-            </a>{" "}
-            or the{" "}
+            </a>{' '}
+            or the{' '}
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               Learning
-            </a>{" "}
+            </a>{' '}
             center.
           </p>
         </div>
@@ -61,5 +62,5 @@ export default function Home() {
         </div>
       </main>
     </div>
-  );
+  )
 }
