53edcdd8fa6100789c833756b09c2b322da5c817
Aleksandr Kopeikin <sundstorm91@gmail.com>
2026-06-13 10:06:20 +0700

    feat: add implementation database with support main methods(readDB, writeDB, findUserByEmail, createUser)

---
+++ lib/db.ts
@@ -0,0 +1,73 @@
+import fs from 'fs';
+import path from 'path';
+
+
+export interface User {
+  id: number
+  email: string
+  password: string
+  name: string
+  role: 'user' | 'admin'
+  createdAt: string
+}
+
+export interface Task {
+  id: number
+  title: string
+  description?: string
+  status: 'pending' | 'in-progress' | 'completed'
+  userId: number
+  deadline?: string
+  createdAt: string
+}
+
+interface DB {
+  users: User[]
+  tasks: Task[]
+}
+
+
+const dbPath = path.join(process.cwd(), 'data', 'db.json');
+
+export function readDB(): DB {
+    const data = fs.readFileSync(dbPath, 'utf-8');
+
+    return JSON.parse(data)
+}
+
+export function writeDB(data: DB) {
+    fs.writeFileSync(dbPath , JSON.stringify(data, null, 2))
+}
+
+export function getUsers() {
+    const db = readDB();
+    return db.users
+}
+
+export function getTasks() {
+    const db: DB = readDB();
+
+    return db.tasks
+}
+
+export function findUserByMail(email: string) {
+    const users = getUsers();
+
+    return users.find(user => user.email === email)
+}
+
+
+export function createUser(userData: Omit<User, 'id'  | 'role' | 'createdAt'>): User {
+    const db = readDB();
+
+    const newUser: User = {
+        id: Date.now(),
+        ...userData,
+        role: 'user',
+        createdAt: new Date().toISOString(),
+  }
+
+  db.users.push(newUser)
+  writeDB(db);
+  return newUser;
+}
